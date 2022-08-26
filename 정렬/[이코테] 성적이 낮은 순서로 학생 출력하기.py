30e9e68347a1967fc59b2ccbbd96e13e420f4e25
ccimayoung <ccimayoung@gmail.com>
2022-08-26 22:47:17 +0900

    [create] 알고리즘 정렬

---
+++ 정렬/[이코테] 성적이 낮은 순서로 학생 출력하기.py
@@ -0,0 +1,12 @@
+
+
+n = int(input())
+data = []
+for i in range(n) :
+    input_data = input().split()
+    data.append((input_data[0], int(input_data[1])))
+
+data.sort(key=lambda x:x[1])
+
+for i in data :
+    print(i[0], end=" ")
